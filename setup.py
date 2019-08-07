20eedf59ac217b2d5735da7579927f279c0a34ee
maksim_work <Saylermb@gmail.com>
2019-08-07 16:13:20 +0300

    gitignore setup.py add discription

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ try:
 except ImportError:
     raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 setup(
-    version='1.02',
+    version='1.03',
     url='',
     description='',
     name='address_templeter',
+    long_description=long_description,
+long_description_content_type= 'text/markdown',
     packages=['address_templeter'],
     package_data={'address_templeter': ['learned_settings.crfsuite']},
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
@@ -19,10 +23,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: Russian',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: Linux :: Ubuntu',
-        'Operating System :: POSIX',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Information Analysis']
